9e23f38d094ebf11b8ccfb96f65565b0710f69b6
Rustam <rustam.tursunzade@gmail.com>
2026-01-14 12:42:48 +0100

    check_heaping_general now can work with .id

--- R/input_diagnostics.R
+++ R/input_diagnostics.R
@@ -16,55 +16,163 @@
 #' }
 #' 
 
+# check_heaping_general <- function(data, y) { 
+#   
+#   tbl <- tibble("level" = c(c("Highly accurate", "Fairly accurate", 
+#                             "Approximate", "Rough","Very rough")),
+#                 color = c("#ffffb2","#fecc5c","#fd8d3c","#f03b20","#bd0026"))
+#   
+#   has_single <- is_single(data$Age)
+#   
+#   if(has_single) {
+#     
+#     # go with 
+#     bachi <- check_heaping_bachi(subset(data, select = y, drop = TRUE),  
+#                                  data$Age, 
+#                                  OAG = TRUE, 
+#                                  method = "pasex")
+#     
+#     myers <- check_heaping_myers(subset(data, select = y, drop = TRUE), data$Age)
+#     
+#     res <- tibble("age scale" = "single",
+#                   "method" = c("bachi", "myers"),
+#                       "result" = c(bachi, myers)) |> 
+#       mutate("level" = cut(.data$result, 
+#                   breaks = c(0,1,2,5,15,80),
+#                   labels = c("Highly accurate", "Fairly accurate", "Approximate",
+#                              "Rough","Very rough")) |> as.character()) |> 
+#       left_join(tbl, by = join_by("level"))
+#     
+#   } 
+#     tblr <- tibble("method" = "roughness",
+#                    "level"  = c(1.03))
+#   # 5-year methods
+#   roughness <- check_heaping_roughness(subset(data, select = y, drop = TRUE), data$Age, ageMin = 30)
+#   sawtooth  <- check_heaping_sawtooth( subset(data, select = y, drop = TRUE), data$Age, ageMin = 30)
+# 
+#   r <- cut(roughness, breaks = c(0,.1,.2,.5,1.5,10),
+#            labels = c("Highly accurate", "Fairly accurate", "Approximate",
+#                       "Rough","Very rough")) |> as.character()
+#   
+#   s <- cut(sawtooth, breaks = c(1,1.03,1.1,1.5,3,10),
+#            labels = c("Highly accurate", "Fairly accurate", "Approximate",
+#                       "Rough","Very rough")) |> as.character()
+#   
+#     res5 <- tibble('age scale' = "5-year",
+#                    "method"    = c("roughness", "sawtooth"),
+#                    "result"    = c(roughness, sawtooth),
+#                    "level"     = c(r, s)) |> 
+#       left_join(tbl, by = join_by("level"))
+#     
+#   if (has_single){
+#     
+#     out <- bind_rows(res, res5)
+#     
+#   } else {
+#     
+#     out <- res5
+#     
+#   }
+#   
+#   return(out)
+#   
+# }
 check_heaping_general <- function(data, y) { 
   
-  tbl <- tibble("level" = c(c("Highly accurate", "Fairly accurate", 
-                            "Approximate", "Rough","Very rough")),
-                color = c("#ffffb2","#fecc5c","#fd8d3c","#f03b20","#bd0026"))
+  if(!(".id" %in% colnames(data))) {
+    data <- data |>
+      mutate(.id = "all")
+  }
   
-  has_single <- is_single(data$Age)
+  labels <- c(
+    "Highly accurate",
+    "Fairly accurate",
+    "Approximate",
+    "Rough",
+    "Very rough"
+  )
+  
+  tbl <- tibble("level" = labels,
+                color = c("#ffffb2", 
+                          "#fecc5c", 
+                          "#fd8d3c", 
+                          "#f03b20", 
+                          "#bd0026"))
+  
+  has_single <- is_single(unique(data$Age))
   
   if(has_single) {
     
-    # go with 
-    bachi <- check_heaping_bachi(subset(data, select = y, drop = TRUE),  
-                                 data$Age, 
-                                 OAG = TRUE, 
-                                 method = "pasex")
-    
-    myers <- check_heaping_myers(subset(data, select = y, drop = TRUE), data$Age)
-    
-    res <- tibble("age scale" = "single",
-                  "method" = c("bachi", "myers"),
-                      "result" = c(bachi, myers)) |> 
+    res <- data |>
+      select("Age", all_of(y), ".id") |>
+      group_nest(.data$`.id`) |>
+      mutate("bachi" = map(data, ~ 
+                             check_heaping_bachi(
+                               Value  = pull(.x, !!sym(y)),
+                               Age    = .x$Age,
+                               OAG    = TRUE,
+                               method = "pasex")),
+             "myers" = map(data, ~
+                             check_heaping_myers(
+                               Value = pull(.x, !!sym(y)),
+                               Age   = .x$Age)),
+             "age scale" = "single") |>
+      select(".id", "bachi", "myers", "age scale") |>
+      unnest(c("bachi", "myers")) |>
+      pivot_longer(-c(".id", "age scale"),
+                   names_to  = "method",
+                   values_to = "result") |>
+      group_by(.data$`.id`) |> 
       mutate("level" = cut(.data$result, 
-                  breaks = c(0,1,2,5,15,80),
-                  labels = c("Highly accurate", "Fairly accurate", "Approximate",
-                             "Rough","Very rough")) |> as.character()) |> 
-      left_join(tbl, by = join_by("level"))
+                           breaks = c(0, 1, 2, 5, 15, 80),
+                           labels = labels),
+             "level" = as.character(.data$level)) |> 
+      left_join(tbl, by = c("level"))
     
-  } 
-    tblr <- tibble("method" = "roughness",
-                   "level"  = c(1.03))
+  }
+  
   # 5-year methods
-  roughness <- check_heaping_roughness(subset(data, select = y, drop = TRUE), data$Age, ageMin = 30)
-  sawtooth  <- check_heaping_sawtooth( subset(data, select = y, drop = TRUE), data$Age, ageMin = 30)
-
-  r <- cut(roughness, breaks = c(0,.1,.2,.5,1.5,10),
-           labels = c("Highly accurate", "Fairly accurate", "Approximate",
-                      "Rough","Very rough")) |> as.character()
+  classify <- function(x, method, breaks, labels) {
+    tibble(
+      result = x,
+      method = method,
+      level  = cut(x, breaks = breaks, labels = labels)
+    )
+  }
   
-  s <- cut(sawtooth, breaks = c(1,1.03,1.1,1.5,3,10),
-           labels = c("Highly accurate", "Fairly accurate", "Approximate",
-                      "Rough","Very rough")) |> as.character()
+  res5 <- data |>
+    select("Age", all_of(y), ".id") |>
+    group_nest(.data$`.id`) |>
+    mutate(
+      "roughness" = map(data, ~ 
+                          check_heaping_roughness(
+                            Value  = pull(.x, !!sym(y)),
+                            Age    = .x$Age,
+                            ageMin = 30)),
+      "sawtooth" = map(data, ~ 
+                         check_heaping_sawtooth(
+                           Value  = pull(.x, !!sym(y)),
+                           Age    = .x$Age,
+                           ageMin = 30)),
+      "res" = map2(.x = roughness, 
+                   .y = sawtooth, ~ 
+                     bind_rows(
+                       classify(
+                         x      = .x,
+                         method = "roughness",
+                         breaks = c(0, .1, .2, .5, 1.5, 10),
+                         labels = labels),
+                       classify(
+                         x      = .y,
+                         method = "sawtooth",
+                         breaks = c(1, 1.03, 1.1, 1.5, 3, 10),
+                         labels = labels))),
+      "age scale" = "5-year") |>
+    select(-c("data", "roughness", "sawtooth")) |>
+    unnest("res") |> 
+    left_join(tbl, by = join_by("level"))
   
-    res5 <- tibble('age scale' = "5-year",
-                   "method"    = c("roughness", "sawtooth"),
-                   "result"    = c(roughness, sawtooth),
-                   "level"     = c(r, s)) |> 
-      left_join(tbl, by = join_by("level"))
-    
-  if (has_single){
+  if(has_single) {
     
     out <- bind_rows(res, res5)
     
