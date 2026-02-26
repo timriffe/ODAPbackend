55ffd89b14c4d7a2641238240c709d58cdd69814
timriffe <tim.riffe@gmail.com>
2026-02-26 10:37:09 +0100

    .

---
+++ ui_flow_control_graduate_standalone.R
@@ -0,0 +1,27 @@
+
+# TODO
+
+# this should show the setup for a graduation-only standalone module.
+# here there are two families, depending on whether we are graduating
+# time or graduating age. They should be in the same module, but which 
+# backend funcition is called should depend on whether the user has given
+# time as the abscissa or age. The main reason why we have two functions 
+# rather than a single wrapper is that the methods lists are different 
+# for age and time.
+
+# see graduate_time()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
